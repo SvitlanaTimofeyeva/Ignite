02551f5df101cacbbbde3b0fe791697e286ca8df
NotMe1994 <moggeth@yandex.ru>
2016-10-15 18:49:13 +0300

    update

---
+++ ReactJS/lesson_24/010_lesson24_code_splitting/build/2.bundle.js
@@ -0,0 +1,58 @@
+webpackJsonp([2],{
+
+/***/ 237:
+/*!*****************************!*\
+  !*** ./jsx/views/view2.jsx ***!
+  \*****************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+	
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+	
+	var React = __webpack_require__(/*! react */ 1);
+	
+	var View2 = function (_React$Component) {
+	    _inherits(View2, _React$Component);
+	
+	    function View2() {
+	        _classCallCheck(this, View2);
+	
+	        return _possibleConstructorReturn(this, (View2.__proto__ || Object.getPrototypeOf(View2)).apply(this, arguments));
+	    }
+	
+	    _createClass(View2, [{
+	        key: 'render',
+	        value: function render() {
+	            return React.createElement(
+	                'div',
+	                null,
+	                React.createElement(
+	                    'h3',
+	                    null,
+	                    'Text 2'
+	                ),
+	                React.createElement(
+	                    'p',
+	                    null,
+	                    'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.'
+	                )
+	            );
+	        }
+	    }]);
+	
+	    return View2;
+	}(React.Component);
+	
+	module.exports.default = View2;
+
+/***/ }
+
+});
+//# sourceMappingURL=2.bundle.js.map
